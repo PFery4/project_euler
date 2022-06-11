86d6452a9759dbb9737aeee2ff87d68871ce9723
PFery4 <paulfery4@gmail.com>
2022-06-11 21:19:31 +0200

    solution: 12

--- solutions/euler_problem_012.py
+++ solutions/euler_problem_012.py
@@ -10,26 +10,24 @@ def triangle_number(idx):
 
 
 def find_divisors(n):
-    divisors = [1, n]
-
-    if n % 2 == 0:
-        divisors.extend([2, n//2])
-    for i in range(3, n//3+1, 2):
+    divisors = []
+    i = 1
+    while i <= n**0.5:
         if n % i == 0:
-            divisors.extend([i, n//i])
-
-    return list(dict.fromkeys(sorted(divisors)))
+            if n / i == i:
+                divisors.append(i)
+            else:
+                divisors.extend([i, n//i])
+        i += 1
+    return divisors
 
 
 if __name__ == '__main__':
 
-    divisor_list = []
-    i = 0
+    num = 1
+    triang = triangle_number(num)
+    while len(find_divisors(triang)) < 500:
+        num += 1
+        triang = triangle_number(num)
 
-    while len(divisor_list) < 500:
-        i += 500
-        triang = triangle_number(i)
-        divisor_list = find_divisors(triang)
-        print(i, triang, len(divisor_list))
-
-    print(divisor_list)
+    print(triang)
