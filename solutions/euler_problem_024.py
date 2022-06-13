b84a94e1af5a9d284ee413db1ebdb1915d6ba3ee
PFery4 <paulfery4@gmail.com>
2022-06-13 21:11:41 +0200

    solution: 24

--- solutions/euler_problem_024.py
+++ solutions/euler_problem_024.py
@@ -4,6 +4,26 @@ https://projecteuler.net/problem=24
 
 """
 
+def str_permutations(string):
+    if len(string) == 0:
+        return []
+    if len(string) == 1:
+        return [string]
+
+    l = []
+    for i in range(len(string)):
+        m = string[i]
+        rem_str = string[:i] + string[i+1:]
+
+        for p in str_permutations(rem_str):
+            l.append(m + p)
+    return l
+
+
 if __name__ == '__main__':
 
-    pass
+    for i, p in enumerate(str_permutations('0123456789')):
+        if i == 999_999:
+            print(p)
+
+
