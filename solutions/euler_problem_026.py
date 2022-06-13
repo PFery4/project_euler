ffc3ebdfeb5a6dfc7b20ab580a1d71ae6e0021f1
PFery4 <paulfery4@gmail.com>
2022-06-13 13:15:02 +0200

    wip: 26

--- solutions/euler_problem_026.py
+++ solutions/euler_problem_026.py
@@ -5,25 +5,33 @@ https://projecteuler.net/problem=26
 """
 
 
-def long_division(n, d):
-    decimals = []
-    already_seen = []
-
+def decimal_representation(n, d):
     div, remainder = divmod(n, d)
-    decimals.append(str(div))
 
-    while remainder != 0 and div not in already_seen[:-1]:
+    first_div = str(div) + '.'
+
+    decimals = ''
+    index = None
+
+    while remainder != 0:
         remainder *= 10
+        while remainder < d:
+            remainder *= 10
+            decimals += '0'
         div, remainder = divmod(remainder, d)
-        if div in already_seen[:-1]:
+        if str(div) in decimals:
+            index = decimals.index(str(div))
             break
-        else:
-            already_seen.append(div)
-            decimals.append(str(div))
+        decimals += str(div)
 
-    return decimals
+    if index is not None:
+        decimals = decimals[:index] + "(" + decimals[index:] + ")"
+
+    return first_div + decimals
 
 
 if __name__ == '__main__':
 
-    print(long_division(1, 7))
+    for i in range(1,500):
+        print(decimal_representation(1, i))
+
