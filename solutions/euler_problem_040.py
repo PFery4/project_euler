40f4e8feb79ffb7f37992f44e08db5c1748e41d4
PFery4 <paulfery4@gmail.com>
2022-06-14 16:58:04 +0200

    solution: 40

---
+++ solutions/euler_problem_040.py
@@ -0,0 +1,20 @@
+"""
+
+https://projecteuler.net/problem=40
+
+"""
+
+if __name__ == '__main__':
+
+    integer = 0
+    decimal = "0."
+
+    while len(decimal) < 1_000_002:     # accounting for the 2 characters at start: '0.'
+        integer += 1
+        decimal += str(integer)
+
+    result = 1
+    for i in range(7):
+        result *= int(decimal[10**i+1])     # again, accounting for the characters at start
+
+    print(result)
