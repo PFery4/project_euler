5a9d3228f8b8d709154a4af20a8d64fb659e7c7c
PFery4 <paulfery4@gmail.com>
2022-07-15 21:06:27 +0200

    mini commit

--- solutions/euler_problem_031.py
+++ solutions/euler_problem_031.py
@@ -5,17 +5,16 @@ https://projecteuler.net/problem=31
 """
 
 if __name__ == '__main__':
-    
+
     currency_elements = [1, 2, 5, 10, 20, 50, 100, 200] 	# in unit pence, [p]
-    
+
     total = 200
-    
+
     ways = [0] * (total+1)
     ways[0] = 1
-    
+
     for x in currency_elements:
-    	for i in range(x, total+1):
-    		ways[i] += ways[i-x]
-    
+        for i in range(x, total+1):
+            ways[i] += ways[i-x]
+
     print(ways[-1])
-    
