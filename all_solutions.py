020224054b0a7540b7cb61236960c4089f0347ae
Paul Féry <paulfery4@gmail.com>
2022-09-10 23:56:48 +0200

    make all_solutions.py more presentable

--- all_solutions.py
+++ all_solutions.py
@@ -12,10 +12,6 @@ if __name__ == "__main__":
     scripts = sorted(glob.glob('./solutions/*.py'))
     scripts = [os.path.basename(file)[:-3] for file in scripts if os.path.isfile(file) and not file.endswith('__init__.py')]
 
-    print(scripts)
-    print(euler_problem_001.solution())
-    print(euler_problem_002.solution())
     for script in scripts:
-        #exec('print(f"{script}.solution()")')
-        print(eval(f"{script}.solution()"))
+        print(f"Solution to problem {script[-3:]}:", eval(f"{script}.solution()"))
 
