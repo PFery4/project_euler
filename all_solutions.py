03dad1116db6a5b51186a501274fae2d71c83ec6
Paul Féry <paulfery4@gmail.com>
2022-11-15 10:33:25 +0100

    format all_solutions file to provide cleaner output (and solving time), create euler problem 33 file

--- all_solutions.py
+++ all_solutions.py
@@ -6,12 +6,23 @@ This script goes through all solutions.
 
 import glob
 import os
+import time
 from solutions import *
 
 if __name__ == "__main__":
     scripts = sorted(glob.glob('./solutions/*.py'))
     scripts = [os.path.basename(file)[:-3] for file in scripts if os.path.isfile(file) and not file.endswith('__init__.py')]
 
+
+    print("PROJECT EULER SOLUTION SPOILERS:\n")
+
+    colwidths = [10, 20, 10]
+    print(f"{'#'.ljust(colwidths[0])}|{'SOLUTION'.ljust(colwidths[1])}|{'TIME [ms]'.ljust(colwidths[2])}")
+
     for script in scripts:
-        print(f"Solution to problem {script[-3:]}:", eval(f"{script}.solution()"))
+        
+        time_0 = time.perf_counter_ns()
+        solution = eval(f"{script}.solution()")
+        duration = (time.perf_counter_ns() - time_0) / 1e6
+        print(f"{script[-3:].ljust(colwidths[0])}|{str(solution).ljust(colwidths[1])}|{str(duration).ljust(colwidths[2])}")
 
