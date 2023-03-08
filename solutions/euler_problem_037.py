d39bc6f23b4ca96a1503ef933fee709346f5f886
Paul Féry <paulfery4@gmail.com>
2023-03-08 19:45:30 +0100

    description method for problem 37

--- solutions/euler_problem_037.py
+++ solutions/euler_problem_037.py
@@ -6,6 +6,12 @@ https://projecteuler.net/problem=37
 
 from euler_problem_003 import is_prime
 
+"""
+numbers we are interested in must only contain digits 1, 3, 7 and 9
+2, 4, 6, 8, 0 are even and therefore cannot be used to make up a number with the desired property.
+5 cannot be used because a number ending with 5 is not prime.
+"""
+
 
 def right_truncatable(number):
     temp_num = str(number)
@@ -28,13 +34,16 @@ def left_truncatable(number):
     
     return not rejected
 
+
 def solution():
 
     truncatable_primes = []
     
     # CHANGE STRATEGY, START WITH PRIMES AND ADD PRIMES TO THEIR EDGES
-    
+    print(right_truncatable(3797))
+    print(left_truncatable(3797))
     return "WIP"
+
 
 if __name__ == "__main__":
     print(solution())
