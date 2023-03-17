7198acf213361b9f67b8d2ec42c9ac0a8695c17f
Paul Féry <paulfery4@gmail.com>
2023-03-17 17:01:10 +0100

    solution problem 39

---
+++ solutions/euler_problem_039.py
@@ -0,0 +1,46 @@
+"""
+
+https://projecteuler.net/problem=39
+
+This solution is really slow, essentially a brute force of the problem, would maybe require optimisation somewhere...
+
+"""
+
+
+def get_triangles(p):
+    """
+    generates the list of valid triangles for perimeter of length p
+    """
+    triangles = []
+    
+    c = p // 2
+    
+    while c > 1:
+        ab = p - c
+        for a in range(1, ab//2):
+            b = ab - a
+            print(a, b, c)
+
+            if a * a + b * b == c * c:
+                triangles.append((a, b, c))
+        c -= 1
+    
+    return triangles
+
+def solution():
+
+    best_p = 120
+    num_p = 3
+
+    for p in range(1, 1001):
+        num_trial = len(get_triangles(p))
+        if num_trial > num_p:
+            num_p = num_trial
+            best_p = p
+
+    return best_p
+
+
+if __name__ == "__main__":
+    print(solution())
+
