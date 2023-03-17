e167cdf1d7fd8766211942afb2b82eeb5656c34c
Paul Féry <paulfery4@gmail.com>
2023-03-17 17:34:31 +0100

    work on problem 359, seems fun

---
+++ solutions/euler_problem_359.py
@@ -0,0 +1,20 @@
+"""
+
+https://projecteuler.net/problem=359
+
+"""
+
+
+from math import sqrt
+
+
+def P(f, r):
+    
+
+
+def solution():
+    return "WIP"
+
+
+if __name__ == '__main__':
+    print(solution())
