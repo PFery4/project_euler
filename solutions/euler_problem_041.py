80702989744c0822650e1498d140ea0373cb5fc6
Paul Féry <paulfery4@gmail.com>
2023-03-26 21:15:49 +0200

    solution problem 41

---
+++ solutions/euler_problem_041.py
@@ -0,0 +1,24 @@
+"""
+
+https://projecteuler.net/problem=41
+
+"""
+
+
+from euler_problem_003 import is_prime
+from itertools import permutations
+
+
+def solution():    
+    p = [int(i) for i in "987654321"]
+    while True:
+        k = len(p)
+        for lst in permutations(p, k):
+            candidate = sum([lst[i] * 10 ** (k-1-i) for i in range(k)])
+            if is_prime(candidate):
+                return candidate
+        p.pop(0)
+
+if __name__ == '__main__':
+    print(solution())
+
