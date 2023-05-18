b15de1f709e1f34c7e6e2ecfb66a80cb5a302263
Paul Féry <paulfery4@gmail.com>
2023-05-18 15:30:32 +0200

    solution to problem 43

--- solutions/euler_problem_043.py
+++ solutions/euler_problem_043.py
@@ -4,9 +4,28 @@ https://projecteuler.net/problem=43
 
 """
 
+from itertools import permutations
+
+
+def check_substring_div(nums):
+    for k, prime in enumerate([2, 3, 5, 7, 11, 13, 17]):
+        ddd = nums[k+1] * 100 + nums[k+2] * 10 + nums[k+3]
+        if ddd % prime:
+            return False  
+    return True
+
 
 def solution():
-    return "WIP"
+    
+    total = 0
+    for num_lst in permutations(str(1234567890)):
+        nums = [int(num) for num in num_lst]
+        num = sum([nums[i] * 10 ** (9 - i) for i in range(10)])
+        
+        if check_substring_div(nums):
+            total += num
+
+    return total
 
 
 if __name__ == '__main__':
