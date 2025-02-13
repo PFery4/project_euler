5eddfc114d2a99cec5a05717413092af78d4d788
Paul Féry <paulfery4@gmail.com>
2025-02-13 15:51:56 +0100

    implement a recursive version of the factorial function, import the factorial function from the standard math library.

--- utils/factorial.py
+++ utils/factorial.py
@@ -1,6 +1,17 @@
 
-def factorial(n: int) -> int:
+from math import factorial as math_factorial
+
+
+def recursive_factorial(n: int) -> int:
+    return 1 if n<=1 else n * factorial(n - 1)
+
+
+def iterative_factorial(n: int) -> int:
     product = 1
     for i in range(n, 0, -1):
         product *= i
     return product
+
+
+factorial = math_factorial
+
