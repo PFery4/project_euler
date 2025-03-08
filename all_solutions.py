c3b6c96c7b8d25d0b276f1574358cbd6ba8e9516
Paul Féry <paulfery4@gmail.com>
2025-03-08 12:36:54 +0100

    make script enter 'N/A' rows for problems that have not been solved yet.

--- all_solutions.py
+++ all_solutions.py
@@ -1,7 +1,5 @@
 """
-
 This script goes through all solutions.
-
 """
 
 import glob
@@ -9,20 +7,25 @@ import os
 import time
 from solutions import *
 
+
+COLWIDTHS = [3, 20, 12]
+NA = "N/A"
+
+
 if __name__ == "__main__":
-    scripts = sorted(glob.glob('./solutions/*.py'))
-    scripts = [os.path.basename(file)[:-3] for file in scripts if os.path.isfile(file) and not file.endswith('__init__.py')]
+    
+    print("PROJECT EULER SOLUTIONS:\n")
+    print(f"{'#'.ljust(COLWIDTHS[0])} | {'SOLUTION'.ljust(COLWIDTHS[1])} | {'TIME [ms]'.ljust(COLWIDTHS[2])}")
 
-
-    print("PROJECT EULER SOLUTION SPOILERS:\n")
-
-    colwidths = [10, 20, 10]
-    print(f"{'#'.ljust(colwidths[0])}|{'SOLUTION'.ljust(colwidths[1])}|{'TIME [ms]'.ljust(colwidths[2])}")
-
-    for script in scripts:
-        
-        time_0 = time.perf_counter_ns()
-        solution = eval(f"{script}.solution()")
-        duration = (time.perf_counter_ns() - time_0) / 1e6
-        print(f"{script[-3:].ljust(colwidths[0])}|{str(solution).ljust(colwidths[1])}|{str(duration).rjust(12)}")
+    last_problem = int(sorted(glob.glob('./solutions/*.py'))[-1][:-3].split("_")[-1])
+    for problem_number in range(1, last_problem + 1):
+    	t0 = time.perf_counter_ns()
+    	try:
+    	    solution = eval(f"euler_problem_{problem_number:03}.solution()")
+    	    duration = (time.perf_counter_ns() - t0) / 1e6
+    	except:
+    	    solution, duration = NA, NA
+    	print(f"{str(problem_number).ljust(COLWIDTHS[0])} | "
+    	      f"{str(solution).ljust(COLWIDTHS[1])} | "
+    	      f"{str(duration).rjust(COLWIDTHS[2])}")
 
