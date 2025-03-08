5c4e0b898c392e2f88022eff2e612b9e9bdfa0c3
Paul Féry <paulfery4@gmail.com>
2025-03-08 12:41:51 +0100

    optimise solution implementation

--- solutions/euler_problem_023.py
+++ solutions/euler_problem_023.py
@@ -2,37 +2,21 @@
 https://projecteuler.net/problem=23
 """
 
-from utils.find_divisors import find_divisors
+from itertools import combinations_with_replacement
+from utils.find_divisors import divisor_set
 
 
-def is_abundant(n):
-    divisors = find_divisors(n)
-    return sum(divisors[:-1]) > n
+def is_abundant(n: int) -> bool:
+    return sum(divisor_set(n)) > 2 * n
 
 
 def solution():
-    abundants = []
-
-    for i in range(1, 28124):
-        if is_abundant(i):
-            abundants.append(i)
-
-    sum_2_abundants = []
-
-    for i in abundants:
-        for j in abundants:
-            sum_2_abundants.append(i + j)
-
-    sum_2_abundants = sorted(list(dict.fromkeys(sum_2_abundants)))
-
-    non_sum_2_abundants = []
-
-    for i in range(1, 28124):
-        if i not in sum_2_abundants:
-            non_sum_2_abundants.append(i)
-
+    abundants = {i for i in range(1, 28124) if is_abundant(i)}
+    sum_2_abundants = {sum(pair) for pair in combinations_with_replacement(abundants, 2)}
+    non_sum_2_abundants = {i for i in range(1, 28124) if i not in sum_2_abundants}
     return sum(non_sum_2_abundants)
 
 
 if __name__ == '__main__':
     print(solution())
+
