edb0f63adeb390996f37ce2a8253fe7c1fffccdd
Paul Féry <paulfery4@gmail.com>
2025-03-08 12:37:50 +0100

    add function that returns divisors of a number, as a set

--- utils/find_divisors.py
+++ utils/find_divisors.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Set
 
 
 def find_divisors(n: int) -> List[int]:
@@ -9,3 +9,13 @@ def find_divisors(n: int) -> List[int]:
             divisors.extend([i, n//i])
         i += 1
     return sorted(list(dict.fromkeys(divisors)))
+
+def divisor_set(n: int) -> Set[int]:
+    divisors = set()
+    i = 1
+    while i <= n**0.5:
+        if n % i == 0:
+            divisors.update({i, n//i})
+        i += 1
+    return divisors
+
